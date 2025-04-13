87084a762345201107cffafe904ff5009c9cfebf
Wilmer Leon <wjleon@gmail.com>
2025-04-13 19:59:46 +0400

    fixing config 3

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -2,12 +2,12 @@
 
 import * as React from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
-import { DayPicker } from 'react-day-picker';
+import { DayPicker, type DayPickerProps } from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+export type CalendarProps = DayPickerProps;
 
 function Calendar({
   className,
